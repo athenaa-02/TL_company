079eac78addde5bdcbc546bb5b6fbcc8bb28aae2
athenaa-02 <natia.karamnishvili369@law.tsu.ge>
2026-03-03 22:33:53 +0400

    feature: add logo and headingstyle: import fonts

---
+++ src/styles/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  primaryRed: "#8A314B",
+  secondaryRed: "#7D1A37",
+  basic: "#EBB9B8",
+  accentColor: "#C79028",
+  headingColor: "#390516",
+  textBasic: "#666666"
+  
+}
+
+
