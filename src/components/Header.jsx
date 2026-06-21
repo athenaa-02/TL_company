541acfab8aaafa3b7debacd665b9a94a1621dcdf
natia <natia@karamnishvili.com>
2026-06-21 21:42:50 +0400

    feat: add mobile nuv button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,28 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-
-
+import Navigation from "./Navigation";
 
 function Header() {
   return (
     <>
-      {/* <HomeLink to="/" >
-        <LogoContainer>
-          <Logo
-            src="../../public/tl small logo 1.png"
-            alt="TL Company logo"
-          ></Logo>
-          <CompanyName>TLCompany</CompanyName>
-        </LogoContainer>
-      </HomeLink> */}
-
       <header className="h-24 bg-primary-red flex items-center">
         <div className="container flex items-center">
-          <img
-            src="/public/tl_small_logo.webp"
-            className="h-14"
-            alt="tl company logo"
-          />
-          <h1 className="font-montserrat text-3xl font-bold text-basic ">
-            <span className="gold-text font-bold ">TL</span>Company
-          </h1>
+          <Link to={"/"}>
+            <img
+              src="/public/tl_small_logo.webp"
+              className="h-14"
+              alt="tl company logo"
+            />
+          </Link>
+
+          <Link to={"/"}>
+            <h1 className="font-montserrat text-3xl font-bold text-basic ">
+              <span className="gold-text font-bold ">TL</span>Company
+            </h1>
+          </Link>
         </div>
         <div className="">
-          <nav>
-
-          </nav>
+          <Navigation></Navigation>
         </div>
       </header>
     </>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,36 @@
 import { useState } from "react";
-import { href, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 
 function Navigation() {
-  const [isOpen, setIsOpnen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const navLinks = [
-    {name: 'Home', href:'/'},
-    {name: 'About us', href:'/about'},
-    {name: 'Practice Areas', href:'/practice-areas'},
-    {name: 'Blog', href:'/blog'},
-    {name: 'Contact', href:'/contact'},
-  ]
-  return (<>
-  <nav>
-    
-  </nav>
-  </>)
+    { name: "Home", href: "/" },
+    { name: "About us", href: "/about" },
+    { name: "Practice Areas", href: "/practice-areas" },
+    { name: "Blog", href: "/blog" },
+    { name: "Contact", href: "/contact" },
+  ];
+  return (
+    <>
+      <nav>
+        {/* desktop navigation */}
+        <div className="md:flex"></div>
+
+        {/* mobile navigation */}
+
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="md:hidden text-white focus:outline-none "
+        >
+          <Menu size={28} />
+        </button>
+        {/* mobile menu */}
+        
+      </nav>
+    </>
+  );
 }
 
 export default Navigation;
