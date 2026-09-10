4d463bebcccfba04edb800c42e63286bf5e28a66
natia <natia@karamnishvili.com>
2026-09-10 23:21:50 +0400

    add three line only texts from api to cards- with line-clamp-3

--- src/components/lawyers/LawyersGeneral.jsx
+++ src/components/lawyers/LawyersGeneral.jsx
@@ -1,5 +1,6 @@
 import { supabase } from "../../lib/supabaseClient";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function LawyersGeneral() {
   const [lawyers, setLawyers] = useState([]);
@@ -29,15 +30,23 @@ function LawyersGeneral() {
 
   return (
     <>
-      <div className="flex mt-30 mb-30 sm:mt-43.25 sm:mb-43.25 flex-wrap justify-center gap-30 sm:gap-20 lg:gap-30 xl:gap-50  items-center ">
+      <div className="flex mt-30 mb-30 sm:mt-43.25 sm:mb-43.25 flex-wrap justify-center gap-25 sm:gap-15 lg:gap-30 xl:gap-50  items-center ">
         {lawyers.map((lawyer) => (
+          <Link to={"/about"}>
             <div key={lawyer.id} className="card flex flex-col gap-9">
-          <div className="w-74.25 h-113.25 ">
-              <img src={lawyer.photo_url} className="w-full h-full object-cover" alt="" />
+              <div className="w-70 h-108 md:w-74.25 md:h-113.25    ">
+                <img
+                  src={lawyer.photo_url}
+                  className="w-full h-full object-cover"
+                  alt={lawyer.full_name}
+                />
+              </div>
+              <h3 className="font-bold text-accent text-2xl text-center">
+                {lawyer.full_name}
+              </h3>
+              <p className="w-72.5  line-clamp-3"> {lawyer.bio}</p>
             </div>
-            <h3 className="font-bold text-accent text-2xl text-center">{lawyer.full_name}</h3>
-            <div className="w-72.5 h-24.5 "> {lawyer.bio}</div>
-          </div>
+          </Link>
         ))}
       </div>
     </>
